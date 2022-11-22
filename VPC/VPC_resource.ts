779c09fed45c4e442be28f2acbf8bcdd93d0dc00
Cis8 <ema.cis8@gmail.com>
2022-11-22 14:59:36 +0100

    Added pulumi aws-typescript project
    Also a basiliar class for creating a VPC has been added

---
+++ VPC/VPC_resource.ts
@@ -0,0 +1,25 @@
+// CIDR: 10.136.0.0/16
+
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { GetVpcResult } from "@pulumi/aws/ec2";
+
+export class VPC {
+  constructor(){
+  }
+
+  vpc?: aws.ec2.Vpc;
+
+  getName(){return "";}
+
+  CreateVPC() {
+    this.vpc = new aws.ec2.Vpc("vpc_res", {
+      cidrBlock: "10.136.0.0/16",
+      tags: {
+        Name: "main",
+    },
+  });
+  }
+
+
+}
