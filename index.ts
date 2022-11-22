6ec0f9f7d6b4572d055844088376a1e298807a5b
Cis8 <ema.cis8@gmail.com>
2022-11-22 17:46:00 +0100

    updated the import for the VPC class
    Fixed an error with the import

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import { VPC } from './VPC/VPC_resource';
+import { VPC } from './VPC/VPC';
 
 const vpc = new VPC("Custom VPC");
 
