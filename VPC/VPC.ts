06a05613ad643a6928342601d1e1f79c31bb6b4d
Cis8 <ema.cis8@gmail.com>
2022-11-23 20:48:04 +0100

    Added routing for public subnets
    Added the route association to the route table (to the Internet Gateway) for the public subnets

--- VPC/VPC.ts
+++ VPC/VPC.ts
@@ -10,18 +10,27 @@ import { Vpc } from "@pulumi/aws/ec2";
 import { ComponentResource, Output, ResourceOptions } from "@pulumi/pulumi";
 import { threadId } from "worker_threads";
 import { VpcIpv4CidrBlockAssociation } from "@pulumi/aws/ec2/vpcIpv4CidrBlockAssociation";
+import { stringify } from "querystring";
 
 export class VPC extends ComponentResource {
   constructor(name: string, opts?: ResourceOptions) {
     super("VPC:infrastructure:base", name, {}, opts);
+    this.pubSubNets = Array<aws.ec2.Subnet>();
+    this.prvSubNets = Array<aws.ec2.Subnet>();
     this.createVPC();
     this.createIGW();
     this.createAZsSubnets();
+    this.createRouteTable();
+    //this.attachRouteTableToPubSubnets();
 
     this.registerOutputs({
       vpc: this.vpc,
       gw: this.gw,
       availableZones: this.availableZones,
+      prvSubNets: this.prvSubNets,
+      pubSubNets: this.pubSubNets,
+      routeTable: this.routeTable,
+      routeTableAssociation: this.routeTableAssociation,
     })
   }
 
@@ -36,6 +45,14 @@ export class VPC extends ComponentResource {
 
   public availableZones?: pulumi.Output<aws.GetAvailabilityZonesResult>;
 
+  public prvSubNets: aws.ec2.Subnet[];
+
+  public pubSubNets: aws.ec2.Subnet[];
+
+  public routeTable?: aws.ec2.RouteTable;
+
+  public routeTableAssociation?: aws.ec2.RouteTableAssociation;
+
 
   // METHODS
 
@@ -43,7 +60,7 @@ export class VPC extends ComponentResource {
     this.vpc = new aws.ec2.Vpc("vpc_res", {
       cidrBlock: "10.136.0.0/24",
       tags: {
-        Name: "main",
+        Name: "myVPC",
     },
     },{
     parent: this,
@@ -54,7 +71,7 @@ export class VPC extends ComponentResource {
     this.gw = new aws.ec2.InternetGateway("gw", {
       vpcId: this.vpc?.id,
       tags: {
-          Name: "IGW",
+          Name: "myIGW",
       },
     },
     {
@@ -69,7 +86,7 @@ export class VPC extends ComponentResource {
     //this.availableZones.names.apply((azNames: string[]) => {
       let i = 0
       azNames.forEach(azName => {
-        new Subnet(azName + "-pvt-subnet", {
+        this.prvSubNets.push(new Subnet(azName + "-pvt-subnet", {
           vpcId: vpcId,
           availabilityZone: azName,
           cidrBlock: this.pvtSubnetsCidrs[i]
@@ -77,9 +94,9 @@ export class VPC extends ComponentResource {
         {
           parent: this,
         }
-        );
+        ));
 
-        new Subnet(azName + "-pub-subnet", {
+        this.pubSubNets.push(new Subnet(azName + "-pub-subnet", {
           vpcId: vpcId,
           availabilityZone: azName,
           cidrBlock: this.pubSubnetsCidrs[i]
@@ -87,9 +104,52 @@ export class VPC extends ComponentResource {
         {
           parent: this,
         }
-        );
+        ));
         i++;
       });
+
+      // attaching the route table to the sub net
+      this.attachRouteTableToPubSubnets()
     });
   }
+
+  protected createRouteTable() {
+    this.routeTable = new aws.ec2.RouteTable("example", {
+      vpcId: this.vpc!.id,
+      routes: [
+          {
+              cidrBlock: "0.0.0.0/0",
+              gatewayId: this.gw!.id,
+          },
+      ],
+      tags: {
+          Name: "myRouteTable",
+      },
+  },
+  {
+    parent: this,
+  });
+  }
+
+  protected attachRouteTableToPubSubnets(){
+    let i = 0
+    //console.log(`this.pubSubNets length: ${this.pubSubNets.length}`)
+    this.pubSubNets.forEach(subNet => {
+      new aws.ec2.RouteTableAssociation(`${i}-routeTableAssociation`, {
+        subnetId: subNet.id,
+        routeTableId: this.routeTable!.id,
+      });
+      i++
+    });
+  }
+
+  /*protected attachRouteTableToGateway() {
+    this.routeTableAssociation = new aws.ec2.RouteTableAssociation("routeTableAssociation", {
+      gatewayId: this.gw!.id,
+      routeTableId: this.routeTable!.id,
+    },
+    {
+      parent: this,
+    });
+  }*/
 }
